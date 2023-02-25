9af81079e10c53812f7b057e6d56dbfc43484c23
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-02-25 16:04:03 +0530

    Appliaction Aui Addded

---
+++ Machine_Vision/Appliaction.py
@@ -0,0 +1,149 @@
+import threading
+import tkinter
+import tkinter.messagebox
+import PIL
+import customtkinter
+import DesktopControllerApplication
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class ToplevelWindow(customtkinter.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("400x300")
+
+        self.label = customtkinter.CTkLabel(self, text="ToplevelWindow")
+        self.label.pack(padx=20, pady=20)
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.toplevel_window = None
+        self.title("AI Machine Vision")
+        self.geometry(f"{1100}x{580}")
+        #Icon for TopLevel Window
+        # self.after(201, lambda: self.iconbitmap("logo2.png"))
+
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        # images
+       # ImageHome = customtkinter.CTkImage(dark_image=PIL.Image.open("AIExapmple.png"), size=(200, 200))  # Images
+
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Developers",
+                                                 font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.open_toplevel,
+                                                        text="Rizvi Ahmed Abbas")
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event,
+                                                        text="Saad Shaikh Mujab")
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event,
+                                                        text="Ubaid Mukadam")
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+                                                                       values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+                                                               values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+
+        # TabsView
+
+        Pages = customtkinter.CTkTabview(self, width=895, height=565,
+                                         fg_color=("#bdbdc1", "#29292a"), corner_radius=15)  # 262628 alternate Color
+        Pages.add(" Home ")
+        Pages.add(" Desktop Controller ")
+        Pages.add(" Trainer ")
+        Pages.add(" VirtualKeyboard ")
+        Pages.add(" Presentation Controller ")
+        Pages.place(x=190, y=3)
+        Pages._segmented_button.grid(sticky="W")
+
+        HomeLabel0 = customtkinter.CTkLabel(Pages.tab(" Home "), text="AI ",
+                                            font=customtkinter.CTkFont(family="IBM Plex Sans", size=90),
+                                            fg_color="white", text_color="black", corner_radius=10)
+        HomeLabel0.place(x=50, y=46)
+
+        HomeLabel1 = customtkinter.CTkLabel(Pages.tab(" Home "), text=" Machine Vision",
+                                            font=customtkinter.CTkFont(family="IBM Plex Sans", size=30),
+                                            corner_radius=10)
+        HomeLabel1.place(x=155, y=78)
+
+
+        HomeLabel2 = customtkinter.CTkLabel(Pages.tab(" Home "), text=" Diploma Final Year Project",
+                                            font=customtkinter.CTkFont(family="IBM Plex Sans", size=40),
+                                            corner_radius=0)
+        HomeLabel2.place(x=5, y=280)
+
+        self.HomeLabel3 = customtkinter.CTkLabel(Pages.tab(" Home "),
+                                            text="An Artificial Intelligence (AI) All Computer Vision program "
+                                                 "are Intergrated in one Appliaction ",
+                                            font=customtkinter.CTkFont(family="IBM Plex Sans", size=19),
+                                            corner_radius=0)
+        self.HomeLabel3.place(x=15, y=350)
+
+        #App Images
+        self.button_1 = customtkinter.CTkButton(Pages.tab(" Desktop Controller "),
+                                           corner_radius=100,
+                                           text="Start",
+                                           command=self.open_DestopControllerApp)
+        self.button_1.place(x=20, y=20)
+
+        # TextBox
+        self.textbox = customtkinter.CTkTextbox(Pages.tab(" Home "), width=350,height=240)
+        self.textbox.grid(row=0, column=1, padx=(500, 0), pady=(20, 0), sticky="nsew")
+        self.textbox.insert("0.0","About This Project\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, "
+                                                     "sed diam nonumy eirmod tempor invidunt ut labore et dolore "
+                                                     "magna aliquyam erat, sed diam voluptua.\n\n")
+        self.textbox.configure(state="disabled")  # configure textbox to be read-only
+
+
+
+    # Methods
+    def open_input_dialog_event(self):
+        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
+        print("CTkInputDialog:", dialog.get_input())
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+    def sidebar_button_event(self):
+        print("sidebar_button click")
+
+    def open_toplevel(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = ToplevelWindow(self)  # create window if its None or destroyed
+            self.toplevel_window.grab_set()
+
+    def open_DestopControllerApp(self):
+        g = DesktopControllerApplication.main()
+        t = threading.Thread(target=g.start)
+        t.start()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
--- Machine_Vision/DesktopControllerApplication.py
+++ Machine_Vision/DesktopControllerApplication.py
@@ -7,129 +7,131 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-wCam, hCam = 640, 480
-cap = cv2.VideoCapture(0)
-cap.set(3,wCam)
-cap.set(4,hCam)
-pTime = 0
-#cTime = 0
-
-face_mash = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
-detector = htm.handDetector(maxHands=1, detectionCon=0.85, trackCon=0.8)
-
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
-volRange = volume.GetVolumeRange()   #(-63.5, 0.0, 0.5) min max
-
-minVol = -63
-maxVol = volRange[1]
-print(volRange)
-hmin = 50
-hmax = 200
-volBar = 400
-volPer = 0
-vol = 0
-color = (0,215,255)
-
-tipIds = [4, 8, 12, 16, 20]
-mode = ''
-active = 0
-
-pyautogui.FAILSAFE = False
-screen_w, screen_h = pyautogui.size()
-while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-    print(lmList,"List of Landmarks")
-   # print(lmList)
-    fingers = []
-    #_, frame = cap.read()
-    # frame = cv2.flip(frame, 1) this will filp the frame
-    rgb_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    output = face_mash.process(rgb_frame)
-    landmark_points = output.multi_face_landmarks
-    frame_h, frame_w, _ = img.shape
-    # Below code is for eye detection
-    if landmark_points:
-        landmarks = landmark_points[0].landmark
-        # for landmark in landmarks[]: this will capture the whole face
-        for id, landmark in enumerate(landmarks[474:478]):
-            x = int(landmark.x * frame_w)
-            y = int(landmark.y * frame_h)
-            capture = cv2.circle(img, (x, y), 3, (0, 255, 0))
-            print(capture)
-    #########--
-    if len(lmList) != 0:
-
-        #Thumb
-        if lmList[tipIds[0]][1] > lmList[tipIds[0 -1]][1]:
-            if lmList[tipIds[0]][1] >= lmList[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-        elif lmList[tipIds[0]][1] < lmList[tipIds[0 -1]][1]:
-            if lmList[tipIds[0]][1] <= lmList[tipIds[0] - 1][1]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
-
-        for id in range(1,5):
-            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
 
 
-        print(fingers)
-        if (fingers == [0,0,0,0,0]) & (active == 0 ):
-            mode='N'
-        elif (fingers == [0, 1, 0, 0, 0] or fingers == [0, 1, 1, 0, 0]) & (active == 0 ):
-            mode = 'Scroll'
+def  main():
+    wCam, hCam = 640, 480
+    cap = cv2.VideoCapture(0)
+    cap.set(3, wCam)
+    cap.set(4, hCam)
+    pTime = 0
+    # cTime = 0
+
+    face_mash = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+    detector = htm.handDetector(maxHands=1, detectionCon=0.85, trackCon=0.8)
+
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    volRange = volume.GetVolumeRange()  # (-63.5, 0.0, 0.5) min max
+
+    minVol = -63
+    maxVol = volRange[1]
+    print(volRange)
+    hmin = 50
+    hmax = 200
+    volBar = 400
+    volPer = 0
+    vol = 0
+    color = (0, 215, 255)
+
+    tipIds = [4, 8, 12, 16, 20]
+    mode = ''
+    active = 0
+
+    pyautogui.FAILSAFE = False
+    screen_w, screen_h = pyautogui.size()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img, draw=False)
+        print(lmList, "List of Landmarks")
+        # print(lmList)
+        fingers = []
+        # _, frame = cap.read()
+        # frame = cv2.flip(frame, 1) this will filp the frame
+        rgb_frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        output = face_mash.process(rgb_frame)
+        landmark_points = output.multi_face_landmarks
+        frame_h, frame_w, _ = img.shape
+        # Below code is for eye detection
+        if landmark_points:
+            landmarks = landmark_points[0].landmark
+            # for landmark in landmarks[]: this will capture the whole face
+            for id, landmark in enumerate(landmarks[474:478]):
+                x = int(landmark.x * frame_w)
+                y = int(landmark.y * frame_h)
+                capture = cv2.circle(img, (x, y), 3, (0, 255, 0))
+                print(capture)
+        #########--
+        if len(lmList) != 0:
+
+            # Thumb
+            if lmList[tipIds[0]][1] > lmList[tipIds[0 - 1]][1]:
+                if lmList[tipIds[0]][1] >= lmList[tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            elif lmList[tipIds[0]][1] < lmList[tipIds[0 - 1]][1]:
+                if lmList[tipIds[0]][1] <= lmList[tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            for id in range(1, 5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            print(fingers)
+            if (fingers == [0, 0, 0, 0, 0]) & (active == 0):
+                mode = 'N'
+            elif (fingers == [0, 1, 0, 0, 0] or fingers == [0, 1, 1, 0, 0]) & (active == 0):
+                mode = 'Scroll'
+                active = 1
+            elif (fingers == [1, 1, 0, 0, 0]) & (active == 0):
+                mode = 'Volume'
+                active = 1
+            elif (fingers == [1, 1, 1, 1, 1]) & (active == 0):
+                mode = 'Cursor'
+                active = 1
+
+        ############# Scroll 👇👇👇👇##############
+        if mode == 'Scroll':
             active = 1
-        elif (fingers == [1, 1, 0, 0, 0] ) & (active == 0 ):
-             mode = 'Volume'
-             active = 1
-        elif (fingers == [1 ,1 , 1, 1, 1] ) & (active == 0 ):
-             mode = 'Cursor'
-             active = 1
+            #   print(mode)
+            putText(mode)
+            cv2.rectangle(img, (200, 410), (245, 460), (255, 255, 255), cv2.FILLED)
+            if len(lmList) != 0:
+                if fingers == [0, 1, 0, 0, 0]:
+                    # print('up')
+                    # time.sleep(0.1)
+                    putText(mode='U', loc=(200, 455), color=(0, 255, 0))
+                    pyautogui.scroll(300)
 
-############# Scroll 👇👇👇👇##############
-    if mode == 'Scroll':
-        active = 1
-     #   print(mode)
-        putText(mode)
-        cv2.rectangle(img, (200, 410), (245, 460), (255, 255, 255), cv2.FILLED)
-        if len(lmList) != 0:
-            if fingers == [0,1,0,0,0]:
-              #print('up')
-              #time.sleep(0.1)
-                putText(mode = 'U', loc=(200, 455), color = (0, 255, 0))
-                pyautogui.scroll(300)
+                if fingers == [0, 1, 1, 0, 0]:
+                    # print('down')
+                    #  time.sleep(0.1)
+                    putText(mode='D', loc=(200, 455), color=(0, 0, 255))
+                    pyautogui.scroll(-300)
+                elif fingers == [0, 0, 0, 0, 0]:
+                    active = 0
+                    mode = 'N'
+        ################# Volume 👇👇👇####################
+        if mode == 'Volume':
+            active = 1
+            # print(mode)
+            putText(mode)
+            if len(lmList) != 0:
+                if fingers[-1] == 1:
+                    active = 0
+                    mode = 'N'
+                    print(mode)
 
-            if fingers == [0,1,1,0,0]:
-                #print('down')
-              #  time.sleep(0.1)
-                putText(mode = 'D', loc =  (200, 455), color = (0, 0, 255))
-                pyautogui.scroll(-300)
-            elif fingers == [0, 0, 0, 0, 0]:
-                active = 0
-                mode = 'N'
-################# Volume 👇👇👇####################
-    if mode == 'Volume':
-        active = 1
-       #print(mode)
-        putText(mode)
-        if len(lmList) != 0:
-            if fingers[-1] == 1:
-                active = 0
-                mode = 'N'
-                print(mode)
+                else:
 
-            else:
-
-                 #   print(lmList[4], lmList[8])
+                    #   print(lmList[4], lmList[8])
                     x1, y1 = lmList[4][1], lmList[4][2]
                     x2, y2 = lmList[8][1], lmList[8][2]
                     cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
@@ -157,7 +159,7 @@ while True:
                         elif vol >= -11:
                             volN = vol
 
-                #    print(int(length), volN)
+                    #    print(int(length), volN)
                     volume.SetMasterVolumeLevel(vol, None)
                     if length < 50:
                         cv2.circle(img, (cx, cy), 11, (0, 0, 255), cv2.FILLED)
@@ -166,68 +168,69 @@ while True:
                     cv2.rectangle(img, (30, int(volBar)), (55, 400), (215, 255, 127), cv2.FILLED)
                     cv2.putText(img, f'{int(volPer)}%', (25, 430), cv2.FONT_HERSHEY_COMPLEX, 0.9, (209, 206, 0), 3)
 
+        #######################################################################
+        if mode == 'Cursor':
+            active = 1
+            # print(mode)
+            putText(mode)
+            cv2.rectangle(img, (110, 20), (620, 350), (255, 255, 255), 3)
 
-#######################################################################
-    if mode == 'Cursor':
-        active = 1
-        #print(mode)
-        putText(mode)
-        cv2.rectangle(img, (110, 20), (620, 350), (255, 255, 255), 3)
+            if fingers[1:] == [0, 0, 0, 0]:  # thumb excluded
+                active = 0
+                mode = 'N'
+                print(mode)
+            else:
+                if len(lmList) != 0:
+                    x1, y1 = lmList[8][1], lmList[8][2]
+                    w, h = autopy.screen.size()
+                    X = int(np.interp(x1, [110, 620], [0, w - 1]))
+                    Y = int(np.interp(y1, [20, 350], [0, h - 1]))
+                    cv2.circle(img, (lmList[8][1], lmList[8][2]), 7, (255, 255, 255),
+                               cv2.FILLED)  # First Finger (Color)White
+                    cv2.circle(img, (lmList[4][1], lmList[4][2]), 7, (0, 255, 0), cv2.FILLED)  # thumb (Color)Green
+                    cv2.circle(img, (lmList[20][1], lmList[20][2]), 7, (0, 255, 0), cv2.FILLED)  # Pinky (Color)Green
+                    cv2.circle(img, (lmList[16][1], lmList[16][2]), 7, (0, 255, 0), cv2.FILLED)  # Pinky (Color)Green
+                    cv2.circle(img, (lmList[12][1], lmList[12][2]), 7, (0, 255, 0), cv2.FILLED)  # Pinky (Color)Green
 
-        if fingers[1:] == [0,0,0,0]: #thumb excluded
-            active = 0
-            mode = 'N'
-            print(mode)
-        else:
-            if len(lmList) != 0:
-                x1, y1 = lmList[8][1], lmList[8][2]
-                w, h = autopy.screen.size()
-                X = int(np.interp(x1, [110, 620], [0, w - 1]))
-                Y = int(np.interp(y1, [20, 350], [0, h - 1]))
-                cv2.circle(img, (lmList[8][1], lmList[8][2]), 7, (255, 255, 255), cv2.FILLED) # First Finger (Color)White
-                cv2.circle(img, (lmList[4][1], lmList[4][2]), 7, (0, 255, 0), cv2.FILLED)  #thumb (Color)Green
-                cv2.circle(img, (lmList[20][1], lmList[20][2]),7, (0, 255, 0), cv2.FILLED)  # Pinky (Color)Green
-                cv2.circle(img, (lmList[16][1], lmList[16][2]), 7, (0, 255, 0), cv2.FILLED)  # Pinky (Color)Green
-                cv2.circle(img, (lmList[12][1], lmList[12][2]), 7, (0, 255, 0), cv2.FILLED)  # Pinky (Color)Green
+                    if X % 2 != 0:
+                        X = X - X % 2
+                    if Y % 2 != 0:
+                        Y = Y - Y % 2
+                    print(X, Y)
+                    autopy.mouse.move(X, Y)
+                    #  pyautogui.moveTo(X,Y)
+                    if fingers[0] == 0:
+                        cv2.circle(img, (lmList[4][1], lmList[4][2]), 7, (0, 0, 255), cv2.FILLED)  # thumb
+                        pyautogui.click()
 
-                if X%2 !=0:
-                    X = X - X%2
-                if Y%2 !=0:
-                    Y = Y - Y%2
-                print(X,Y)
-                autopy.mouse.move(X,Y)
-              #  pyautogui.moveTo(X,Y)
-                if fingers[0] == 0:
-                    cv2.circle(img, (lmList[4][1], lmList[4][2]), 7, (0, 0, 255), cv2.FILLED)  # thumb
-                    pyautogui.click()
+                    if fingers[4] == 0:
+                        cv2.circle(img, (lmList[20][1], lmList[20][2]), 7, (0, 0, 255), cv2.FILLED)  # thumb
+                        pyautogui.rightClick()
 
-                if fingers[4] == 0:
-                     cv2.circle(img, (lmList[20][1], lmList[20][2]), 7, (0, 0, 255), cv2.FILLED)  # thumb
-                     pyautogui.rightClick()
+                    if fingers[2] == 0:
+                        cv2.circle(img, (lmList[12][1], lmList[12][2]), 7, (0, 0, 255), cv2.FILLED)  # thumb
+                        pyautogui.mouseDown(button="left")
+                        pyautogui.moveTo(X, Y, duration=0.1)
 
-                if fingers[2] == 0:
-                    cv2.circle(img, (lmList[12][1], lmList[12][2]), 7, (0, 0, 255), cv2.FILLED)  # thumb
-                    pyautogui.mouseDown(button="left")
-                    pyautogui.moveTo(X, Y, duration=0.1)
+                    if fingers[3] == 0:
+                        cv2.circle(img, (lmList[16][1], lmList[16][2]), 7, (0, 0, 255), cv2.FILLED)  # thumb
+                        pyautogui.doubleClick()
 
-                if fingers[3] == 0:
-                     cv2.circle(img, (lmList[16][1], lmList[16][2]), 7, (0, 0, 255), cv2.FILLED)  # thumb
-                     pyautogui.doubleClick()
+        # cv2.imshow('Eye Controlled Mouse ', frame)
+        def putText(mode, loc=(250, 450), color=(0, 255, 255)):
+            cv2.putText(img, str(mode), loc, cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                        3, color, 3)
+
+        cTime = time.time()
+        fps = 1 / ((cTime + 0.01) - pTime)
+        pTime = cTime
+
+        cv2.putText(img, f'FPS:{int(fps)}', (480, 50), cv2.FONT_ITALIC, 1, (255, 0, 0), 2)
+        cv2.imshow('Hand LiveFeed', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
 
-    cTime = time.time()
-    fps = 1/((cTime + 0.01)-pTime)
-    pTime = cTime
-
-    cv2.putText(img,f'FPS:{int(fps)}',(480,50), cv2.FONT_ITALIC,1,(255,0,0),2)
-    cv2.imshow('Hand LiveFeed',img)
-
-    #cv2.imshow('Eye Controlled Mouse ', frame)
-    cv2.waitKey(1)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    def putText(mode,loc = (250, 450), color = (0, 255, 255)):
-        cv2.putText(img, str(mode), loc, cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                    3, color, 3)
+if __name__ == "__main__":
+    main()
