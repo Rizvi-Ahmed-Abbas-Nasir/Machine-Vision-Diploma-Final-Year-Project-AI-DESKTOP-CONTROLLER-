a4f3dacc85541a59f511c75595fed304e3ee049d
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-10-02 23:30:39 +0530

    added

--- Machine_Vision/inference_classifier.py
+++ Machine_Vision/inference_classifier.py
@@ -16,7 +16,7 @@ def main():
 
     hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-    labels_dict = {0: 'A', 1: 'B', 2: 'L', 3: 'C', 4: 'D'}
+    labels_dict = {0: 'A', 1: 'B', 2: 'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'K'}
     while True:
 
         data_aux = []
@@ -68,8 +68,6 @@ def main():
                         cv2.LINE_AA)
 
         cv2.imshow('frame', frame)
-        cv2.waitKey(1)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
